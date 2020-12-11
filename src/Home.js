eee27113936b898c6ad9e331a2451167566955a4
Hardik <19it.hardikprajapati@adit.ac.in>
2020-12-11 20:12:42 +0530

    Full Home Page Done

--- src/Home.js
+++ src/Home.js
@@ -10,18 +10,25 @@ function Home() {
                 />
 
                 <div className='home__row'>
-                    <Product title="Soft Skills: The Software Developer's Life Manual (Manning)" price='449' rating={5} image='https://m.media-amazon.com/images/I/51La7RXPopL._AC_UY327_FMwebp_QL65_.jpg' />
-                    <Product />
+
+                    <Product title="Soft Skills: The Software Developer's Life Manual (Manning) by John Z. Sonmez" price='449' rating={4} image='https://m.media-amazon.com/images/I/51La7RXPopL._AC_UY327_FMwebp_QL65_.jpg' />
+
+                    <Product title="DOUBLE R BAGS Front Frame Handlebar Cycle Bag for Men Women Boys Bicycle Mobile Accessories " price='631' rating={5} image='https://m.media-amazon.com/images/I/719iCqIUCuL._AC_UY327_FMwebp_QL65_.jpg' />
+
                 </div>
 
                 <div className='home__row'>
-                    <Product />
-                    <Product />
-                    <Product />
+
+                <Product title="Samsung Galaxy Watch (Bluetooth + LTE, 46 mm) - Silver" price='9999' rating={4} image='https://m.media-amazon.com/images/I/71oNPZKNTfL._AC_UL480_FMwebp_QL65_.jpg' />
+
+                <Product title="Bournvita Cadbury Health Drink, 1kg Jar" price='399' rating={5} image='https://m.media-amazon.com/images/I/51gn+ccasqL._AC_UL480_FMwebp_QL65_.jpg' />
+
+                <Product title="New Apple iPhone 12 Pro (128GB) - Graphite" price='1,19,900' rating={4} image='https://m.media-amazon.com/images/I/71YlH-4MUQL._AC_UY327_FMwebp_QL65_.jpg' />
+
                 </div>
 
                 <div className='home__row'>
-                    <Product />
+                <Product title="Soft Skills: The Software Developer's Life Manual (Manning)" price='1,54,999' rating={5} image='https://m.media-amazon.com/images/I/81V+hqDbomL._AC_UY327_FMwebp_QL65_.jpg' />
                 </div>
             </div>
         </div>
